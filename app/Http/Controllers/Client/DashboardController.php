955be9acaac141f22c93af86e347037f7b56cff8
mamurjor-tarek <mamurjor.tarek@gmail.com>
2024-05-26 11:19:57 +0600

    count

--- app/Http/Controllers/Client/DashboardController.php
+++ app/Http/Controllers/Client/DashboardController.php
@@ -5,26 +5,34 @@ namespace App\Http\Controllers\Client;
 use App\Models\User;
 use App\Models\Voteannounce;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function dashboard()  {
-          // if (Gate::allows('isAdmin')) {
-            $this->setPageTitle('Dashboard');
-            
-            $client = User::where('id', Auth::id())->first();
-            $vote   = Voteannounce::where('tehsil', $client->tehsil)->get();
-       
-            
-        $notifications = $client->notifications()->where('type', 'App\Notifications\VoteAnnouncementNotification')->get();
-        $filters =  $client->notifications()->where('type', 'App\Notifications\WingsVoteAnnouncementNotification')->whereJsonContains('data->user_name', $client->profession)->get();
-        $unreadcount   = $client->unreadNotifications()->count();
-        // $singlevalue = Voteannounce::where('id', Auth::id())->first();
-            return view('backend.include.content_wrapper',compact('unreadcount','notifications','vote','filters'));
-          // } else {
-          //     abort(401);
-          // }
-       }
+  public function dashboard()
+  {
+      $this->setPageTitle('Dashboard');
+      
+      $client = User::where('id', Auth::id())->first();
+      $vote   = Voteannounce::where('tehsil', $client->tehsil)->get();
+  
+      $notifications = $client->notifications()->where('type', 'App\Notifications\VoteAnnouncementNotification')->get();
+      $filters       = $client->notifications()->where('type', 'App\Notifications\WingsVoteAnnouncementNotification')
+          ->whereJsonContains('data->user_name', $client->profession)->get();
+  
+          $hasWingsVoteAnnouncements = $client->notifications()->where('type', 'App\Notifications\WingsVoteAnnouncementNotification')->whereJsonContains('data->user_name', $client->profession)->count() > 0;
+            // dd($hasWingsVoteAnnouncements);
+            // Log::info('Has Wings Vote Announcements: ', ['value' => $hasWingsVoteAnnouncements]);
+          
+            $unreadcount = $hasWingsVoteAnnouncements
+              ?  $client->unreadNotifications()->count() 
+              :  $client->unreadNotifications()->where('type', 'App\Notifications\VoteAnnouncementNotification')->count();
+      
+
+      return view('backend.include.content_wrapper', compact('unreadcount', 'notifications', 'vote', 'filters'));
+   
+  }
+
 }
